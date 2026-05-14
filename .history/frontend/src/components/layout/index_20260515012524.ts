739af2af445ee8989f98d3e8cb79ee5de88290da
vajjipartisatwikraj <satwikrajv@gmail.com>
2026-05-15 02:11:39 +0530

    Added .history to GitIgnore

--- .history/frontend/src/components/layout/index_20260515012524.ts
+++
@@ -1,9 +0,0 @@
-export { Navbar } from "./Navbar";
-export { Sidebar } from "./Sidebar";
-export { Footer } from "./Footer";
-export { Layout } from "./Layout";
-
-export type { default as Navbar } from "./Navbar";
-export type { default as Sidebar } from "./Sidebar";
-export type { default as Footer } from "./Footer";
-export type { default as Layout } from "./Layout";
